7c564e53b53e7781adb52dc9720ad3bf89be28fe
engpk <engpk@tp.edu.sg>
2013-07-09 10:31:09 +0000

    Included part 3 of HW 3

--- grader.js
+++ grader.js
@@ -3,6 +3,8 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler'); 
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -42,8 +44,22 @@ if (require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'URL to index.html')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var fname = "index2.html";
+    if (program.url) {
+	rest.get(program.url).on('complete', function(result, response) {
+	    if (result instanceof Error) {
+		console.error('Error: ' + util.format(response.message));
+	    } else {
+		fs.writeFileSync(fname, result);
+	    }
+	});
+    } else {
+	fname = program.file;
+    }
+    var checkJson = checkHtmlFile(fname, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 }
